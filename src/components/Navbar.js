91dd14951e32fc2c8acbede70d572bda1f002c29
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2021-09-01 11:20:29 -0400

    optimized for production

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BsCodeSlash } from 'react-icons/bs'
 
 export default function Nav() {
     return (
         <React.Fragment>
             <nav className="flex bg-default-black text-neon-teal text-2xl font-jura">
-                <a href="#" className="hover:text-neon-pink flex p-6">Dwayne Devs</a>
-                <a className="hover:text-neon-pink p-6 ml-auto" href="#">About</a>
-                <a className="hover:text-neon-pink p-6" href="#">Contact</a>
+                <a href="https://www.google.com" className="hover:text-neon-pink flex p-6">Dwayne Devs</a>
+                <a href="https://www.google.com" className="hover:text-neon-pink p-6 ml-auto">About</a>
+                <a href="https://www.google.com" className="hover:text-neon-pink p-6">Contact</a>
             </nav>
         </React.Fragment>
     )
