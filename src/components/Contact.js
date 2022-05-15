5636246d565e42f9263c01a288d087e462696f86
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2022-05-15 12:43:07 -0400

    Fixed issues with Projects and Contact sections

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,14 @@
 import { SiTwitter, SiInstagram, SiGithub } from 'react-icons/si';
-import { IoArrowForwardCircleSharp } from 'react-icons/io5'
-import ContactForm from './ContactForm';
 
 
 const Footer = () => {
     return (
         <>
-            <div className="grid grid-cols-2 md:grid-cols-8 h-full xl:h-screen-75 2xl:text-2xl text-lg text-ternary pt-10 md:pt-0 md:mt-10" id="footer">
-                <div className="col-span-2 md:col-span-3 text-white font-regular md:text-3xl flex flex-col items-center justify-center gap-y-10 md:col-start-2" id="contact">
+            <div className="grid grid-cols-4 h-full xl:h-screen-75 2xl:text-2xl text-lg text-ternary pt-10 md:pt-0 md:mt-10" id="footer">
+                <div className="col-span-2 text-white font-regular md:text-3xl flex flex-col items-center justify-center gap-y-10 md:col-start-2" id="contact">
                     <div className="flex flex-col items-center text-center">
                         <span>Like my work?  </span>
                         <span>Then reach out to me and we can begin building your next project!</span>
-                        <span className="mt-2 arrow-bounce self-center hidden md:block" id="arrow"><IoArrowForwardCircleSharp size={64} /></span>
                     </div>
                     <div className="md:flex hidden items-center md:text-4xl md:gap-x-10">
                         <a href="https://www.twitter.com/dwayne_devs" rel="noreferrer" target="_blank" className="flex items-center hover:text-secondary">
@@ -25,10 +22,7 @@ const Footer = () => {
                         </a>
                     </div>
                 </div>
-                <div className="grid lg:grid-cols-4 col-span-2 md:col-span-3 md:items-center my-5 md:my-0">
-                    <ContactForm />
-                </div>
-                <div className="grid col-span-2 py-5 justify-items-center text-xl md:hidden text-white">
+                {/* <div className="grid col-span-2 py-5 justify-items-center text-xl md:hidden text-white">
                         <header className="font-semibold">Connect on Social</header>
                         <div className="flex gap-x-5">
                             <a href="https://www.twitter.com/dwayne_devs" rel="noreferrer" target="_blank" className="flex items-center hover:text-secondary">
@@ -41,7 +35,7 @@ const Footer = () => {
                                 <SiInstagram className="mr-1" />
                             </a>
                         </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SiCss3, SiJavascript, SiNetlify, SiReact, SiTailwindcss } from "react-icons/si";
 import xmas from "../christmas-app.png"
 import knd from '../knights-and-dragons.png'
-import rta from "../RateThatAnime.png"
+import poke from "../pokestox.png"
 
 
 const Projects = () => {
@@ -38,7 +38,7 @@ const Projects = () => {
                     </div>
             </div>
             <div className="flex text-ternary flex-col items-center gap-2 md:gap-4 p-4 col-span-2 md:py-10 h-full shadow-2xl border border-secondary">
-                    <header className="font-secondary text-primary text-xl md:text-4xl">Knights & Dragons (Desktop Only, Mobile coming soon!)</header>
+                    <header className="font-secondary text-primary text-xl md:text-4xl">Knights & Dragons (Desktop Only)</header>
                     <hr className="h-1 bg-secondary w-12 md:w-32" />
                     <img src={knd} alt="" className="hidden p-1 sm:block border border-primary" />
                     <div className="flex justify-center sm:hidden">
@@ -66,12 +66,12 @@ const Projects = () => {
             <div className="flex text-ternary flex-grow flex-col items-center gap-2 md:gap-4 p-4 col-span-2 md:py-10 h-full shadow-2xl border border-secondary">
                     <header className="font-secondary text-primary text-xl md:text-4xl">PokeStox (In progress)</header>
                     <hr className="h-1 bg-secondary w-12 md:w-32" />
-                    <img src={rta} alt="" className="hidden p-1 sm:flex border border-primary" />
+                    <img src={poke} alt="" className="hidden p-1 sm:flex border border-primary" />
                     <div className="flex justify-center sm:hidden">
-                        <img src={rta} alt="" />
+                        <img src={poke} alt="" />
                     </div>
                     <p className="text-center md:text-base text-sm">
-                    A website for anime lovers to discuss, find and rate their favorite anime, manga and light novels!
+                        A web app that will allow users to view and save prices of Pokemon cards in their collection! Users will be able to get real-time appraisals of the worth of their cards.
                     </p>
                     <div className="flex mt-auto flex-col gap-2 items-center md:text-xl font-semibold">
                         Technologies used:
@@ -83,8 +83,8 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="flex gap-2">
-                        <a href="https://www.github.com/dwaynerilljr/knights-and-dragons" className="bg-primary text-white text-center 2xl:text-2xl xl:text-lg border-none rounded-md p-1 xl:p-2 hover:bg-secondary" rel="noreferrer" target="_blank">Github Repo</a>
-                        <a href="https://knights-and-dragons.netlify.app/" className="bg-primary text-white text-center 2xl:text-2xl xl:text-lg border-none rounded-md p-1 xl:p-2 hover:bg-secondary" rel="noreferrer" target="_blank">Live Demo</a>
+                        <a href="https://www.github.com/dwaynerilljr/pokestox" className="bg-primary text-white text-center 2xl:text-2xl xl:text-lg border-none rounded-md p-1 xl:p-2 hover:bg-secondary" rel="noreferrer" target="_blank">Github Repo</a>
+                        {/* <a href="https://knights-and-dragons.netlify.app/" className="bg-primary text-white text-center 2xl:text-2xl xl:text-lg border-none rounded-md p-1 xl:p-2 hover:bg-secondary" rel="noreferrer" target="_blank">Live Demo</a> */}
                     </div>
             </div>
         </div>
